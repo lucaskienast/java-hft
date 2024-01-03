e104543e83dc5af31e20087326f7ec5d3c2b2ca3
LucasKienast <lucas.kienast@hotmail.de>
2024-01-03 13:18:52 +0100

    cleanup

--- src/main/java/org/hft/data/PriceHolder.java
+++ src/main/java/org/hft/data/PriceHolder.java
@@ -8,15 +8,10 @@ import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class PriceHolder {
-    // private static final int STRIPE_SIZE = 12;
 
     private final ThreadLocal<Map<String, BigDecimal>> lastSeenPrices;
     private final Map<String, BigDecimal> namesToPrices;
-
-    private final Map<String, ReentrantReadWriteLock> entityLocks;
-    // downside of striped lock is that all entities that map to the same lock will share the same condition variable
-    // private final ReentrantReadWriteLock[] stripedLocks = new ReentrantReadWriteLock[STRIPE_SIZE];
-
+    private final Map<String, ReentrantReadWriteLock> entityLocks; // downside of striped lock is that all entities that map to the same lock will share the same condition variable
     private final Map<String, Condition> entityConditions;
 
     public PriceHolder() {
@@ -24,7 +19,6 @@ public class PriceHolder {
         entityLocks = new ConcurrentHashMap<>();
         entityConditions = new ConcurrentHashMap<>();
         lastSeenPrices = ThreadLocal.withInitial(ConcurrentHashMap::new);
-        // for (int i = 0; i < STRIPE_SIZE; i++) stripedLocks[i] = new ReentrantReadWriteLock();
     }
 
     /** Called when a price ‘p’ is received for an entity ‘e’ */
@@ -81,38 +75,6 @@ public class PriceHolder {
      * call to getPrice() or waitForNextPrice(), it returns immediately that price.
      * Otherwise, it blocks until the next price change for entity ‘e’.
      */
-    /*
-    public BigDecimal waitForNextPrice(String e) throws InterruptedException {
-        ReentrantReadWriteLock lock = getEntityLock(e);
-        lock.readLock().lock();
-
-        System.out.println(Thread.currentThread().getName() + " - is holding lock to wait for next price");
-
-        try {
-            BigDecimal lastSeenPrice = lastSeenPrices.get().get(e);
-            Condition condition = entityConditions.computeIfAbsent(e, k -> lock.writeLock().newCondition());
-
-            while (true) {
-                BigDecimal currentPrice = namesToPrices.get(e);
-
-                System.out.println(Thread.currentThread().getName() + " - Last seen price=" + lastSeenPrice + ", current price=" + currentPrice);
-
-                if (currentPrice != null && !currentPrice.equals(lastSeenPrice)) {
-                    lastSeenPrices.get().put(e, currentPrice);
-                    return currentPrice;
-                }
-
-                System.out.println(Thread.currentThread().getName() + " - waiting for condition to be signalled, giving up lock...");
-                boolean receivedSignal = condition.await(3, TimeUnit.SECONDS);
-                System.out.println(Thread.currentThread().getName() + " - reacquired lock, did receive signal for new price? -> " + receivedSignal);
-                if (!receivedSignal) return currentPrice;
-            }
-        } finally {
-            lock.readLock().unlock();
-        }
-    }
-     */
-
     public BigDecimal waitForNextPrice(String e) throws InterruptedException {
         ReentrantReadWriteLock lock = getEntityLock(e);
         lock.writeLock().lock();
@@ -145,24 +107,10 @@ public class PriceHolder {
     }
 
     private ReentrantReadWriteLock getEntityLock(String e) {
-        // return stripedLocks[getStripeIndex(e)];
         return entityLocks.computeIfAbsent(e, k -> new ReentrantReadWriteLock(true));
     }
 
-    /*
-    private int getStripeIndex(String e) {
-        return Math.abs(e.hashCode() % STRIPE_SIZE);
-    }
-     */
-
     private Condition getLockCondition(String e, LockType lockType) {
-        /*
-        ReentrantReadWriteLock lock = stripedLocks[getStripeIndex(e)];
-        if (lockType == LockType.WRITE_LOCK) return lock.writeLock().newCondition();
-        else if (lockType == LockType.READ_LOCK) return lock.readLock().newCondition();
-        else throw new RuntimeException("Invalid LockType");
-         */
-
         return entityConditions.computeIfAbsent(e, k -> {
             if (lockType == LockType.WRITE_LOCK)
                 return entityLocks.get(e).writeLock().newCondition();
